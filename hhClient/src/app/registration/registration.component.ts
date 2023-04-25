68bb33cccfa232c3174b6d047519bd06437e8058
arooj khan <aroojkhan.wr@gmail.com>
2023-04-25 11:39:17 -0400

    registration form checks requirement
    even chk email regex!!

--- hhClient/src/app/registration/registration.component.ts
+++ hhClient/src/app/registration/registration.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import {UserRegistrationService} from "../user-registration.service";
 import {User} from "../user";
-import {Observable} from "rxjs";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
 
 
 @Component({
@@ -12,11 +13,13 @@ import {Observable} from "rxjs";
 })
 export class RegistrationComponent implements OnInit {
 
+
   user: User=new User(0,"","","","",0,"","","");
   confirmPassword:string;
   message:any;
 
   isRegistered: boolean = false;
+  registrationForm: FormGroup;
 
   canRegister:boolean =false;
 
@@ -24,9 +27,13 @@ export class RegistrationComponent implements OnInit {
   toggleIsRegistered() {
     this.isRegistered = !this.isRegistered;
   }
-  constructor(private service:UserRegistrationService) { }
+  constructor(private service:UserRegistrationService,private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.registrationForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+
   }
 
 
@@ -39,13 +46,28 @@ export class RegistrationComponent implements OnInit {
       this.canRegister=false;
     }
   }
+
   public registerNow(){
+
+    if (this.user.password.length < 8 || this.user.password.length > 20) {
+      alert("Password must be between 8 and 20 characters.");
+      return;
+    }
     let resp=this.service.doRegistration(this.user);
     resp.subscribe((data)=>this.message=data);
   }
 
 
-
+  public emailIsValid() {
+    const email = document.getElementById("email") as HTMLInputElement;
+    const emailValue = email.value;
+    if (typeof emailValue === "string") {
+      const re =new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+      return re.test(emailValue);
+    } else {
+      return false;
+    }
+  }
 }
 
 
